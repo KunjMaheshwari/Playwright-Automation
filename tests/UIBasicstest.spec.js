2070fd332d428abffa236de83a7941b5d05e2794
Kunj Maheshwari <kunjm@outlook.com>
2026-06-06 10:53:42 +0530

    Update login test to use specific username and handle error message

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -5,7 +5,7 @@ test.only('First playwright test', async({browser})=>{
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
-    await page.locator("//input[@id='username']").fill("rahulshettyacademy");
+    await page.locator("//input[@id='username']").fill("kunj");
 
     await page.locator("//input[@id='password']").fill("Learning@830$3mK2");
 
@@ -15,7 +15,10 @@ test.only('First playwright test', async({browser})=>{
 
     await page.click("//input[@id='signInBtn']");
 
-    await page.waitForTimeout(3000);
+    //handling error message
+    const errorMessage = await page.locator("//div[contains(@style, 'block')]");
+    await errorMessage.waitFor();
+    console.log(await errorMessage.textContent);
 });
 
 test('Page playwright test', async({page})=>{
