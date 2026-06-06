34f934d2981e9d27b143211e0387a446b25513bd
Kunj Maheshwari <kunjm@outlook.com>
2026-06-06 10:43:59 +0530

    Refactor first Playwright test to use browser context and enhance login functionality

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,27 @@
 import {test, expect} from "@playwright/test";
 
-test('First playwright test', async({browser})=>{
+test.only('First playwright test', async({browser})=>{
     const context =  await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    await page.locator("//input[@id='username']").fill("rahulshettyacademy");
+
+    await page.locator("//input[@id='password']").fill("Learning@830$3mK2");
+
+    await page.locator("//select[@class='form-control']").selectOption('Teacher');
+
+    await page.click("//input[@id='terms']");
+
+    await page.click("//input[@id='signInBtn']");
+
+    await page.waitForTimeout(3000);
 });
 
 test('Page playwright test', async({page})=>{
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.goto("https://google.com");
+
+    await expect(page).toHaveTitle("Google");
+
+    await page.waitForTimeout(5000);
 });
