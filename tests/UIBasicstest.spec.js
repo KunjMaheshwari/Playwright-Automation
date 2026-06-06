9660df992e3d2e6c5d7dd57ab2d827472932eef1
Kunj Maheshwari <kunjm@outlook.com>
2026-06-06 11:10:20 +0530

    Update login test to use correct username and select 'Student' role

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -5,20 +5,37 @@ test.only('First playwright test', async({browser})=>{
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
-    await page.locator("//input[@id='username']").fill("kunj");
+    await page.locator("//input[@id='username']").fill("rahulshettyacademy");
 
     await page.locator("//input[@id='password']").fill("Learning@830$3mK2");
 
-    await page.locator("//select[@class='form-control']").selectOption('Teacher');
+    await page.locator("//select[@class='form-control']").selectOption('Student');
 
     await page.click("//input[@id='terms']");
 
     await page.click("//input[@id='signInBtn']");
 
     //handling error message
-    const errorMessage = await page.locator("//div[contains(@style, 'block')]");
-    await errorMessage.waitFor();
-    console.log(await errorMessage.textContent);
+    // const errorMessage = await page.locator("//div[contains(@style, 'block')]");
+    // await errorMessage.waitFor();
+    // console.log(await errorMessage.textContent());
+
+    const pageTitle = page.locator("(//h4[@class='card-title'])[1]");
+
+    await pageTitle.scrollIntoViewIfNeeded();
+
+    // Highlight element
+    await pageTitle.evaluate((el) => {
+        el.style.border = '3px solid red';
+        el.style.backgroundColor = 'yellow';
+    });
+
+    await pageTitle.waitFor({ state: 'visible' });
+
+    const productText = await pageTitle.textContent();
+    console.log(productText);
+
+    await expect(pageTitle).toHaveText('iphone X');
 });
 
 test('Page playwright test', async({page})=>{
